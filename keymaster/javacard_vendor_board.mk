240c7d78874059185bda47bed93051765d9b2cb5
Bhuvan Varshney <bvarshne@codeaurora.org>
2021-09-28 19:19:41 +0530

    Revert "eSE-SB: Disable javacard service build on Waipio"
    This reverts commit 3e1386c02637ba304d55da1b5ca50fe9eb1b5f08.
    
    This change re-enables eSE-StrongBox HAL service compilation.
    
    Change-Id: If1382f987fcf63a8479d1bebaeebec12e33200ec

--- keymaster/javacard_vendor_board.mk
+++ keymaster/javacard_vendor_board.mk
@@ -1,6 +1,6 @@
 #Taro specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),taro)
-TARGET_USES_ESESB := false
+TARGET_USES_ESESB := true
 ENABLE_SB_SPLIT_MANIFEST := false
 ENABLE_SB_FEATURE_XML := false
 endif
