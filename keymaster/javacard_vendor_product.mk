0b1659b6777dd401fa48c4ceb76143ddaeabe290
Bhuvan Varshney <quic_bvarshne@quicinc.com>
2021-11-23 15:40:14 +0530

    eSE-SB: KM: Add strongbox keystore permissions for diwali
    eSE specific feature XML file need to be made part of
    Diwali platforms only.
    
    Add rules to build strongbox feature XML file for
    Diwali, and skip Taro platform.
    
    Change-Id: I1419ce4ba38516234c86e81485f07b87df5d5fd3

--- keymaster/javacard_vendor_product.mk
+++ keymaster/javacard_vendor_product.mk
@@ -8,10 +8,14 @@ ifeq ($(strip $(ENABLE_SB_SPLIT_MANIFEST)),true)
 ESE_VENDOR_SB += android.hardware.keymaster@4.1-javacard.service.xml
 endif
 
-ifeq ($(strip $(ENABLE_SB_FEATURE_XML)),true)
-ESE_VENDOR_SB += android.hardware.strongbox_keystore.xml
-endif
-
 ifeq ($(strip $(TARGET_USES_ESESB)),true)
 PRODUCT_PACKAGES += $(ESE_VENDOR_SB)
+
+ifeq ($(strip $(ENABLE_SB_FEATURE_XML)),true)
+# Copy feature XML file on defined SKUs
+PRODUCT_SKUS := diwali
+PRODUCT_COPY_FILES += \
+    $(foreach PRODUCT_SKU, $(PRODUCT_SKUS), \
+    vendor/nxp/opensource/keymaster/keymaster/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_$(PRODUCT_SKU)/android.hardware.strongbox_keystore.xml)
+endif
 endif
