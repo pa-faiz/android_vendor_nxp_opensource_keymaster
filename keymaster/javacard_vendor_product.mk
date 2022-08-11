3af6221c84eccdb269f79730ad8bc89382b7cdc7
Bhuvan Varshney <quic_bvarshne@quicinc.com>
2022-08-11 11:20:09 +0530

    eSE-SB: KM4.1: Add build rules for KM4.1 support
    Add build rules to support Keymaster 4.1 module
    enablement on Kalama platform.
    
    Remove unwanted platforms and build rules for
    older targets.
    
    Change-Id: I6a88ecfea5fe7448282926a80595f1d6e7e7f8a8

--- keymaster/javacard_vendor_product.mk
+++ keymaster/javacard_vendor_product.mk
@@ -10,16 +10,7 @@ endif
 
 ifeq ($(strip $(TARGET_USES_ESESB)),true)
 PRODUCT_PACKAGES += $(ESE_VENDOR_SB)
-
 ifeq ($(strip $(ENABLE_SB_FEATURE_XML)),true)
-# Copy feature XML file on defined SKUs
-PRODUCT_SKUS := diwali
-PRODUCT_COPY_FILES += \
-    $(foreach PRODUCT_SKU, $(PRODUCT_SKUS), \
-    vendor/nxp/opensource/keymaster/keymaster/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_$(PRODUCT_SKU)/android.hardware.strongbox_keystore.xml)
-
-ifeq ($(call is-board-platform-in-list, parrot),true)
 PRODUCT_COPY_FILES += vendor/nxp/opensource/keymaster/keymaster/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml
-endif
 endif
 endif
