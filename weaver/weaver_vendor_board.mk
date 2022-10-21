b30dd195413d5ced2a9f68ea84a2d4f228b158ef
PRANAY BHARGAV BHAVARAJU <quic_pbhavara@quicinc.com>
2022-10-21 15:02:14 +0530

    eSE-SB: Added lanai specific flag for weaver and keymint Compilation
    Added pineapple specific flag to support weaver and keymint Compilation
    
    Change-Id: I3965ffce38cb8b927f895f09577268478b495b7c

--- weaver/weaver_vendor_board.mk
+++ weaver/weaver_vendor_board.mk
@@ -1,5 +1,5 @@
-#Kalama specific build rules
-ifeq ($(TARGET_BOARD_PLATFORM),kalama)
+#pineapple specific build rules
+ifeq ($(TARGET_BOARD_PLATFORM),pineapple)
 TARGET_USES_ESE_WEAVER := false
 ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
