9d6d3332c23ca19b39f369fc84f6d952d4c5a6de
Bhuvan Varshney <quic_bvarshne@quicinc.com>
2022-11-14 23:10:45 +0530

    eSE-SB: Add build rules to support SPF targets
    Add build rules to support keymint solution
    on SPF targets aligned with kalama
    
    Change-Id: I9abe50e4a1e34d3bcc4c8be664b866686733a182

--- keymint/keymint_vendor_product.mk
+++ keymint/keymint_vendor_product.mk
@@ -1,10 +1,13 @@
 # Build Keymaster open source vendor modules
 ESE_VENDOR_KEYMINT += android.hardware.security.keymint-service.strongbox
-ESE_VENDOR_KEYMINT += hal_uuid_map_config.xml
 
 ifeq ($(strip $(ENABLE_ESE_KEYMINT_SPLIT_MANIFEST)),true)
 ESE_VENDOR_KEYMINT += android.hardware.security.keymint-service.strongbox.xml
 ESE_VENDOR_KEYMINT += android.hardware.security.sharedsecret-service.strongbox.xml
+endif
+
+ifeq ($(strip $(ENABLE_ESE_KEYMINT_FEATURE_XML)),true)
+ESE_VENDOR_KEYMINT += android.hardware.strongbox_keystore.xml
 endif
 
 ifeq ($(strip $(TARGET_USES_ESE_KEYMINT)),true)
