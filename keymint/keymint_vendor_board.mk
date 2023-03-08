d1606e094d30454a52f4ac9ae020e2465e2e9fa9
Harish Potluri <quic_hpotluri@quicinc.com>
2023-03-08 20:51:08 +0530

    eSE-SB: Remove conditional start of keymint eSE SB HAL
    Remove conditional start of keymint eSE SB HAL based on vendor.ese.strongbox property
    
    Tests:
    Keystore strongbox CTS testcase
    
    Change-Id:I900159108fef8195febd3f4ec4c5337b1c96b845

--- keymint/keymint_vendor_board.mk
+++ keymint/keymint_vendor_board.mk
@@ -1,6 +1,6 @@
 #Kalama specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),kalama)
-TARGET_USES_ESE_KEYMINT := true
+TARGET_USES_ESE_KEYMINT := false
 ENABLE_ESE_KEYMINT_SPLIT_MANIFEST := false
 ENABLE_ESE_KEYMINT_FEATURE_XML := false
 endif
