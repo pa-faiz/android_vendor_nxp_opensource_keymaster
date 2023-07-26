92dd0411d4545c78133ee454ad53a8e2414080fa
Abhishek Dhumal <quic_adhumal@quicinc.com>
2023-07-26 11:31:09 +0530

    eSE-Strongbox: enablement for crow
    Enable keymint and weaver on crow target.
    
    Change-Id: Ie7fad56be5ac4a3f717a318ab3513d15d11bb44e

--- weaver/weaver_vendor_board.mk
+++ weaver/weaver_vendor_board.mk
@@ -3,3 +3,9 @@ ifeq ($(TARGET_BOARD_PLATFORM),kalama)
 TARGET_USES_ESE_WEAVER := true
 ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
+
+#Camano specific build rules
+ifeq ($(TARGET_BOARD_PLATFORM),crow)
+TARGET_USES_ESE_WEAVER := true
+ENABLE_ESE_WEAVER_SPLIT_MANIFEST := true
+endif
