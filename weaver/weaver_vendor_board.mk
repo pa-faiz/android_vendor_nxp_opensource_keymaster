7e697b03d976867023faca3cb279eeabf8a2b497
Om Prakash Singh <quic_omprsing@quicinc.com>
2023-09-11 20:28:47 +0530

    weaver: disable weaver for kalama
    eSE-Strongbox and weaver is not POR for kalama hence disable it
    by default.

--- weaver/weaver_vendor_board.mk
+++ weaver/weaver_vendor_board.mk
@@ -1,6 +1,6 @@
 #Kalama specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),kalama)
-TARGET_USES_ESE_WEAVER := true
+TARGET_USES_ESE_WEAVER := false
 ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
 
