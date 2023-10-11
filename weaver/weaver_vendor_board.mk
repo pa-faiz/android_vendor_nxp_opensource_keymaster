060982a837c7030778889deabea504f9b0ff6034
PRANAY BHARGAV BHAVARAJU <quic_pbhavara@quicinc.com>
2023-10-12 00:04:20 +0530

    Keymint,weaver: Added sun flag in the  build rules
    As SNXXX strongbox is supported on sun, added the build rules for the same.
    
    Change-Id: I1e43efca243909d74f34718e15f4320e15b23766

--- weaver/weaver_vendor_board.mk
+++ weaver/weaver_vendor_board.mk
@@ -3,3 +3,9 @@ ifeq ($(TARGET_BOARD_PLATFORM),pineapple)
 TARGET_USES_ESE_WEAVER := false
 ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
+
+#sun specific build rules
+ifeq ($(TARGET_BOARD_PLATFORM),sun)
+TARGET_USES_ESE_WEAVER := false
+ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
+endif
