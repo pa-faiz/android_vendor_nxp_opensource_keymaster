cccfe188a848b3578c2726f4f13318e3e47e553d
PRANAY BHARGAV BHAVARAJU <quic_pbhavara@quicinc.com>
2023-10-16 10:08:41 +0530

    Disabling keymint and weaver on crow target.
    As per the internal alignement, it is decided to disable keymint and weaver.
    
    Change-Id: I1a618b31698224d927ad4684561cccf941f12a8d

--- weaver/weaver_vendor_board.mk
+++ weaver/weaver_vendor_board.mk
@@ -6,6 +6,6 @@ endif
 
 #Camano specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),crow)
-TARGET_USES_ESE_WEAVER := true
-ENABLE_ESE_WEAVER_SPLIT_MANIFEST := true
+TARGET_USES_ESE_WEAVER := false
+ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
