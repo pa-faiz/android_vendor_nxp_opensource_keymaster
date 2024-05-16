8c696f4bac5f9d355f6c99125374146e5a5e91bf
Shivangi Kesharwani <quic_skesharw@quicinc.com>
2024-05-16 22:13:13 +0530

    Rename authsecret service
    Rename authsecret service to avoid build conflict with AOSP code.
    
    Change-Id: I61dcbe00ce4433ccb0d8e9f2d03616f68548775d

--- keymint/keymint_vendor_product.mk
+++ keymint/keymint_vendor_product.mk
@@ -1,6 +1,6 @@
 # Build Keymaster open source vendor modules
 ESE_VENDOR_KEYMINT += android.hardware.security.keymint-service.strongbox-nxp
-ESE_VENDOR_KEYMINT += android.hardware.authsecret-service.nxp
+ESE_VENDOR_KEYMINT += android.hardware.authsecret-service.nxp-qti
 
 ifeq ($(strip $(ENABLE_ESE_KEYMINT_SPLIT_MANIFEST)),true)
 ESE_VENDOR_KEYMINT += android.hardware.security.keymint-service.strongbox.xml
